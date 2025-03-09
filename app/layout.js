202ca6171575c277d8ce7d2cf0b7f95a5ed12f8e
colin <colin@endsley.co>
2025-03-08 19:09:46 -0800

    Ensure chat widget script is properly loaded

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,6 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         {children}
-        {/* Abacus.AI Chat Widget */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
